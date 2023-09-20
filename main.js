daf40a1139952760c729232d2eed24de846bc785
kenanhomsi <kinanhomsi0959@gmail.com>
2023-09-20 14:41:54 +0300

    first

---
+++ main.js
@@ -0,0 +1,555 @@
+let y=document.getElementsByTagName('h1');
+let h1_text=y[0].innerText;
+if(h1_text==='خدمات الفعاليات والمشاريع'){
+    console.log(yess);
+}
+/* Add custom Js code below */
+let main=document.getElementsByClassName('main-content')[0];
+main.innerHTML=' ';
+
+main.innerHTML=`
+<div class="video" style="text-align: center;" id="How">
+<video width="100%" height="40%" controls>
+    <source src="https://drive.google.com/file/d/17bI6ea44PUbkZVSOrVh5QmQuCB5M2Iaq/v" type="video/mp4" />
+   </video>
+</div>
+<div class="container">
+<div class="title section_margin">
+مشاريع الخدمات و الفعاليات
+</div>
+
+<div class="first-section section_margin">
+<img src="https://i.ibb.co/f1pmxZk/11-1.jpg" alt="">
+<div class="txt"> 
+    <div>
+    <h2>                      طباعة هويات داخلية
+    </h2>
+    <p>         طباعة هويات داخلية للشركات تعتبر جزءًا مهمًا من استراتيجية إدارة العلامات التجارية وتنظيم البيئة الداخلية للشركة التي تسهم بدورها لتذكير الموظفين بقيم الشركة ورسالتها. فنحن في دكو ميديا نضمن لكم جودة المواد المستخدمة ودقة تفاصيل طباعة الرسومات والنصوص.
+    </p>
+</div>
+<div>
+    <h2>                      طباعة هويات خارجية
+    </h2>
+    <p>         طباعة هويات داخلية للشركات تعتبر جزءًا مهمًا من استراتيجية إدارة العلامات التجارية وتنظيم البيئة الداخلية للشركة التي تسهم بدورها لتذكير الموظفين بقيم الشركة ورسالتها. فنحن في دكو ميديا نضمن لكم جودة المواد المستخدمة ودقة تفاصيل طباعة الرسومات والنصوص.
+    </p>
+</div>
+</div>
+<img src="https://i.ibb.co/0sBNWTN/6-1.jpg" alt="">
+
+
+</div>
+
+
+
+
+
+<div class="second-section section_margin ">
+<img src="https://i.ibb.co/1qpWB8c/circule.jpg" alt="">
+<div>
+    <h2> شاشات ليد</h2>
+    <p>
+        تعد شاشات ليد من التقنيات المتطورة لدى دكو ميديا، فنحن نقدمها بدقة عرض مثالية مع وجود سطوع عالي وتباين جيد تواكب سرعة تغيرات المحتوى بالفيديو المعروض بكل احترافية ووضوح.
+
+    </p>
+</div>
+</div>
+<div class="threed-section section_margin">
+
+<img id="img2" src="https://i.ibb.co/HDRNW1z/7-3.jpg" alt="">
+
+
+<div>
+    <h2> تصنيع و تغطية الحواجز الامنية    </h2><br>
+    <p> تكمن أهمية تغطية السياج الحديدية بزيادة الوعي بالفعالية وجذب الانتباه، ومن الممكن توجيه الجمهور إلى نقاط معينة بداخل الفعالية واستغلال المساحة لعرض شعارات الشركات والرعاة، بالاضافة لكونها تعكس الروح العام للفعالية
+        <span>نصنع لكم السياج الحديدية والمزيد في مجال صناعة الهياكل المعدنية واللحام لخلق قطع معدنية مختلفة تتناسب مع الحدث
+
+        </span>
+        تكمن أهمية تغطية السياج الحديدية بزيادة الوعي بالفعالية وجذب الانتباه، ومن الممكن توجيه الجمهور إلى نقاط معينة بداخل الفعالية واستغلال المساحة لعرض شعارات الشركات والرعاة، بالاضافة لكونها تعكس الروح العام للفعالية، مما يساهم في 
+    
+    </p>
+    <img id="img1" src="https://i.ibb.co/GkDZz1h/Untitled-1.jpg" alt="">
+</div>
+</div>
+<div class="ideas">
+<div class="center-title">
+    <div class="title-left"></div>
+    <div class="title-right"></div>
+</div>
+<div class="center-squer">
+    <img src="https://i.ibb.co/LYnjwqK/logo.webp" alt="">
+</div>
+<div class="idea">
+    <div class="lines"></div>
+    <img src="https://i.ibb.co/bbTBW7C/t-Ue6q-TQJkr-DOuae5hrt-JOpe8-Xrz-Drhtk65z7-OJ3p.jpg" alt="">
+    <div class="text">
+        <h2>التصميم</h2>
+        <p>نقدم خدمات تصميم مبتكرة واحترفية تلبي احتياجاتكم وتحوّل رؤيتكم إلى حقيقة ملموسة
+
+        </p>
+    </div>
+</div>
+<div class="idea-2">
+    <div class="lines"></div>
+    <img src="https://i.ibb.co/r2R8MPp/sgJMPq.jpg" alt="">
+    <div class="text">
+        <h2>الطباعة</h2>
+        <p>نوفر خدمات طباعة عالية الجودة مع أحدث التقنيات والمعدات لضمان جودة الطباعة والتفاصيل الدقيقة
+        </p>
+    </div>
+</div>
+<div class="idea-3">
+    <div class="lines"></div>
+    <img src="https://i.ibb.co/sQ2SGjk/2e.jpg" alt="">
+    <div class="text">
+        <h2>التنفيذ</h2>
+        <p>نقدم خدمات تنظيم شاملة لفعالياتكم، وادارة كل التفاصيل بتوفير الحلول المبتكرة لضمان نجاح مشروعاتكم
+
+        </p>
+    </div>
+</div>
+</div>
+<div class="line section_margin">
+<div class="section four-section ">
+    <img src="https://i.ibb.co/nLL5CzJ/4-3.jpg" alt="">
+    <div>
+        <h2>     طباعة لوحات عاكسة
+        </h2>
+            <p>
+        اللوحات العاكسة في دكو ميديا تعد من المنتجات المهمة للسلامة، حيث تساعد في الرؤية بوضوح خلال الليل أو في الظروف القليلة بالاضاءة.   
+
+            </p>                    
+    </div>
+</div>
+<div class="section  five-section ">
+    <img src="https://i.ibb.co/6DYdtqy/8-1.jpg" alt="">
+    <div class="t">
+        <h2> طباعة اكريليك بتقنية UV</h2>
+            <p>
+                تتميز طباعات الـ UV على الاكريليك لدى دكو ميديا بكونها طبقة حبر متينة ومقاومة للخدش والتآكل، مما يجعهلها مناسبة للاستخدام الداخلي والخارجي
+            </p>
+    </div>
+ </div>
+
+<div class="section six-section">
+    <img src="https://i.ibb.co/cvp4hsx/5-1.jpg" alt="">
+    <div>
+        <h2>   لوحات نيون</h2>
+            <p>
+في دكو ميديا نوفر خدمة إنشاء اللوحات الاعلانية والفنية باستخدام أنابيب زجاجية مضيئة، إذ أنها تعد من اللمسات الفنية والابداعية، فهي متخصصة بمجال العمل الزجاجي والمضيء لينتج عنها لوحة مميزة تجذب الانظار.
+
+            </p>
+    </div>
+ </div>
+</div>
+<div class="section seven-section section_margin ">
+<img src="https://i.ibb.co/chM8rB8/7-1.jpg" alt="">
+<div>
+    <h2>    أكشاك المعارض</h2>
+        <p>
+            أكشاك المعارض تعتبر عملية تصميم وإنتاج الهياكل واللوحات والمواد الدعائية المطبوعة بشكل احترافي التي تستخدم في الفعاليات لتمثيل شركة وعلامة تجارية، إذ تساعد على إنشاء مظهر مميز وجذاب للجناح الذي يعرض المنتج او الخدمة.
+        </p>
+</div>
+</div>
+<div class="section eight-section section_margin">
+<img src="https://i.ibb.co/wJdKcv1/2-2.jpg" alt="">
+<div>
+    <h2> تصميم وطباعة السيارة</h2>
+        <p>
+            فهي وسيلة تسويق متنقلة تزيد من الوعي بالعلامة التجارية بسبب تواجدها الدائم بالمقارنة مع وسائل التسويق الاخرى.
+        </p>
+</div>
+</div>
+<div class="section nine-section section_margin">
+<img src="https://i.ibb.co/Cv1w3dT/49.png" alt="">
+<div>
+    <h2>   طباعة لوحات الطرق - طباعة لوحات مرورية</h2>
+        <p>
+            لوحات الطرق الحديدية تعد ضمن خدماتنا في الحدادة التي نصنعها بكل احترافية، فهي تسهم بشكل كبير في تحسين نظام النقل وزيادة السلامة على الطرق وتقديم المعلومات اللازمة للسائقين والمارة
+        </p>
+</div>
+</div>
+<div class="section ten-section section_margin">
+<img src="https://i.ibb.co/SPKTJD0/9-3.jpg" alt="">
+<div>
+    <h2>   ورش عمل وحدادة</h2>
+        <p>
+            في ورش عمل دكو ميديا نقدم لكم كل ماتحتاجون من أعمال الحدادة وصناعة الهياكل المعدنية واللحام لخلق قطع معدنية مختلفة تتناسب مع الحدث.
+
+
+        </p>
+</div>
+</div>
+</div>
+`
+css=`/* Add custom CSS styles below */
+:root{
+    --main-color:#FFBD22;
+    --second-color:#882A80;
+    --threed-color:#F04B94;
+}
+
+*{
+    box-sizing: border-box;
+}
+body{
+    margin: 0;
+    padding: 0;
+}
+.video{
+    width: 100%;
+}
+
+.section_margin{
+    margin: 90px 0px;
+}
+.title{
+    width: 80%;
+    text-align: center;
+    background-color: var(--second-color);
+    margin-right: 10%;
+    padding-top: 10px;
+    height: 65px;
+    font-size: 30px;
+    color: white;
+    border-top-right-radius: 20px;
+    border-bottom-left-radius: 20px;
+    position: relative;
+}
+ .title::after{
+content: '';
+position: absolute;
+width: 99%;
+height: 10px;
+bottom: 0;
+background-color: var(--main-color);
+right: 0;
+border-bottom-left-radius: 20px;
+ }
+
+.first-section{
+display: flex;
+
+}
+.first-section img{
+    width: 500px;
+    height: 400px;
+    border-top-left-radius: 90px;
+    border-bottom-right-radius: 90px;
+    border: 3px solid var(--main-color);
+    
+}
+.first-section .txt{
+    display: flex;
+    flex-direction: column;
+}
+.first-section .txt h2{
+font-size:21px;}
+.first-section .txt p{
+font-size:12px;
+}
+.first-section .txt div:first-child{
+    justify-content: flex-start;
+    position: relative;
+}
+.first-section .txt div:first-child::after{
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 3px;
+    bottom: 0;
+    right: 0;
+    background-color: var(--main-color);
+}
+.first-section .txt div {
+    padding:0px 10px;
+}
+.first-section .txt div:last-child{
+text-align: end;}
+.first-section .txt div h2{
+    color: var(--second-color);
+}
+
+
+
+
+
+
+.second-section{
+    display: flex;
+    flex-direction: row-reverse;
+}
+.second-section img{
+    border-top-right-radius: 90px;
+    border-bottom-left-radius: 90px ;
+    border-top-left-radius: 30px;
+    border-bottom-right-radius:  30px;
+    border: 3px solid var(--second-color);
+}
+.second-section div{
+text-align: center;
+    margin-left: 30px;
+    margin-top: 90px;
+    position: relative;
+
+}
+.second-section div::after{
+    content: '';
+    position: absolute;
+    width: 90%;
+    height: 3px;
+    background-color: var(--main-color);
+    bottom:40%;
+    right: 0;
+}
+
+
+.second-section div h2{
+    font-size: 27px;
+    color: var(--main-color);
+       margin-bottom: 15px;
+
+}
+.second-section div p{
+    font-size: 15px;
+
+}
+.threed-section{
+    display: flex;
+       flex-direction: row-reverse;
+}
+.threed-section div{
+    display: flex;
+    flex-direction: column;
+    
+    justify-content: center;
+    position: relative;
+    gap:5px;
+
+}
+
+
+.threed-section div h2{
+    position: relative;
+    font-size: 36px;
+    color: var(--main-color);
+}
+.threed-section div h2::after{
+    content: '';
+    position: absolute;
+    width: 90%;
+    height: 3px;
+    background-color: var(--main-color);
+    bottom: -30px;
+    right: 0;
+}
+.threed-section div p{
+   font-size: 17px;
+}
+.threed-section div p span{
+    color: var(--threed-color);
+}
+#img1{
+    width: 500px;
+    height: 350px;
+}
+#img2{
+    width: 550px;
+    transform: rotateZ(90deg);
+
+
+}
+
+.line{
+    display: grid;
+    grid-template-columns: 30% 30% 30%;
+    gap: 5%;
+    align-items: center;
+    text-align: center;
+    position: relative;
+
+}
+.line img{
+    width: 400px;
+    height: 310px;
+}
+
+.line .section{
+    width: 104%;
+    overflow: hidden;
+    background-color: var(--main-color);
+    border: 6px solid var(--main-color);
+    transition:5s;   
+}
+ .line .section > div{
+    transition:5s;   
+    display: none;
+} 
+
+.line .five-section {
+    background-color: var(--second-color);
+    border: 6px solid var(--second-color);
+}
+
+ .line .section:hover > div{
+    display: block;
+}
+
+.line .section div h2{
+
+    color: var(--second-color);
+}
+.line .five-section div h2 {
+    color: var(--main-color);
+}
+.line .section div p{
+    font-size: 20px;
+    color: white;
+}
+.ideas{
+    width: 100%;
+    position: relative;
+    height: 70vh;
+    background-color: #F04B94;
+}
+.center-squer{
+    width: 350px;
+    height: 350px;
+    background-color: rgb(255, 189, 34);
+    position: absolute;
+    top: 64%;
+    left: 38%;
+    transform: rotateZ(45deg) translate(-50%, -50%);
+}
+.center-squer img{
+    width: 300px;
+    margin-left: 23px;
+    margin-top: 20px;
+
+}
+.center-title{
+    position: absolute;
+    top: 40%;
+    right: 20%;
+    font-size: 100px;
+    justify-content: space-between;
+    background-color: var(--main-color);
+    display: flex;
+    gap: 150x;
+    width: 60%;
+    height: 120px;
+}
+.idea ,.idea-2,.idea-3{
+    position: absolute;
+    bottom: 5px;
+    right: 5px;
+}
+.idea-2{
+    left: 5px;
+}
+.idea-3{
+    top: 100px;
+    right: 150px; 
+}
+.idea .lines,.idea-2 .lines,.idea-3 .lines{
+    width: 50px;
+    height: 100px;
+    top: -73px;
+    right: 215px;
+    position: absolute;
+    border-bottom-left-radius: 30px;
+    border-bottom-right-radius: 30px;
+    border: 5px dashed white;
+    border-top: none;
+    
+}
+.idea-3 .lines{
+    width: 190px;
+    height: 50px;
+        right: 213px;
+    
+    top: -85px;
+    border-radius: 0;
+    border:none;
+    border-bottom: 5px dashed white;
+}
+.idea .lines{
+    border-top-left-radius: -10px;
+    border-right: none;
+    top: -103px;
+    right: 250px;
+    
+}
+.idea-2 .lines {
+    border-top-right-radius: -10px;
+    right: 950px;
+    border-left: none;
+}
+.idea img,.idea-2 img,.idea-3 img{
+    position: absolute;
+      right: 121px;
+    top: -60px;
+
+    width: 150px;
+    border-radius: 50%;
+    border: 2px solid var(--main-color);
+
+}
+.idea-2 img{
+    right: 995px;
+}
+.idea-3 img{
+right:130px;
+}
+.idea .text,.idea-2 .text,.idea-3 .text{
+    position: relative;
+    right: 20px;
+    top: -190px;
+    width: 200px;
+    color: white;
+}
+.idea-3 .text{
+   top: -55px;
+    right: 624px;
+}
+.idea-2 .text{
+    right: 995px;
+
+    text-align: end;
+    
+}
+.idea .text h2,.idea-2 .text h2,.idea-3 .text h2{
+    color: var(--main-color);
+    font-size: 23px;
+}
+
+.seven-section,.nine-section{
+    display: flex;
+    flex-direction: row;
+    gap: 50px;
+}
+.seven-section img, .nine-section img{
+    border: 3px solid var(--main-color);
+    border-top-right-radius: 90px;
+    border-bottom-left-radius: 90px;
+
+}
+.seven-section div,.nine-section div,.eight-section div,.ten-section div{
+    margin-top: 30px;
+}
+.seven-section div h2,.nine-section div h2,.eight-section div h2,.ten-section div h2{
+    color: var(--main-color);
+}
+.seven-section div p,.nine-section p,.eight-section div p,.ten-section div p{
+    font-size: 20px;
+}
+.eight-section,.ten-section{
+    display: flex;
+    flex-direction: row-reverse;
+    gap: 50px;
+}
+.eight-section img, .ten-section img{
+    border: 3px solid var(--main-color);
+    border-top-left-radius: 90px;
+    border-bottom-right-radius: 90px;
+
+}`
